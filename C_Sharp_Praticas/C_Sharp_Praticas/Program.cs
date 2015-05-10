50862cb1784f83b7922e0eba87e3da704a880432
devos1 <devos1@bluewin.ch>
2015-05-10 21:53:58 +0200

    Initial commit

---
+++ C_Sharp_Praticas/C_Sharp_Praticas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Praticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("John", 5.2, 4.1);
+            Console.WriteLine("{0,-20} {1,-10}", "Student", "Final Note");
+            Console.WriteLine("{0,-20} {1,7}", student.name, student.ClassFinal());
+            Console.WriteLine("------------------------------------------------------");
+            student.CompleteClassFinal();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C_Sharp_Praticas/C_Sharp_Praticas/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Praticas
+{
+    class Student
+    {
+        // PROPERTIES
+        public String name { get; set; }
+        public Double test1 { get; set; }
+        public Double test2 { get; set; }
+
+        // CONSTRUCTOR
+        public Student(String Name, Double Test1, Double Test2)
+        {
+            name = Name;
+            test1 = Test1;
+            test2 = Test2;
+        }
+
+        /// <summary>
+        /// Imprime les 2 notes avec la moyenne
+        /// </summary>
+        public void CompleteClassFinal(){
+            Console.WriteLine("Note 1 : {0} ; Note 2 : {1} => Moyenne : {2}",test1, test2, Math.Round(((test1+test2)/2),1,MidpointRounding.AwayFromZero));
+        }
+
+        /// <summary>
+        /// Retourne la moyenne des notes 
+        /// </summary>
+        /// <returns>AVG Note</returns>
+        public Double ClassFinal()
+        {
+            return Math.Round(((test1 + test2) / 2), 1, MidpointRounding.AwayFromZero);
+        }
+
+
+    }
+}
